fcbfc1d8d003e3756eec5a6c2dfab7dd1c9ad216
BenjVA <ben.valette@live.fr>
2023-08-01 16:46:05 +0200

    Logout successfully

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogoutController extends AbstractController
+{
+    /**
+     * @throws Exception
+     */
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): never
+    {
+        $this->addFlash('successLogout', 'Vous avez bien été déconnecté!');
+        // controller can be blank: it will never be called!
+        throw new Exception('Should never be reached');
+    }
+}
