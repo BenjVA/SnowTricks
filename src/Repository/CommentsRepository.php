4d7b6294b2c1de9c69296316f77ebb26272fe0da
BenjVA <113171110+BenjVA@users.noreply.github.com>
2023-08-29 12:14:42 +0200

    displays correctly comments by trick, paginated by 10, adding icons to delete or edit them, create methods in next branch (#46)

--- src/Repository/CommentsRepository.php
+++ src/Repository/CommentsRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Comments;
 use App\Entity\Tricks;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,9 +18,26 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentsRepository extends ServiceEntityRepository
 {
+    const PAGINATOR_PER_PAGE = 10;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comments::class);
+    }
+
+    public function getCommentsPaginated(Tricks $tricks, int $offset): Paginator
+    {
+        $query = $this->getEntityManager()->createQueryBuilder()
+            ->select('c')
+            ->from('App\Entity\Comments', 'c')
+            ->where("c.tricks = :tricks")
+            ->setParameter("tricks", $tricks)
+            ->setFirstResult($offset)
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->orderBy('c.createdAt', 'desc')
+            ->getQuery();
+
+        return new Paginator($query);
     }
 
 //    /**
