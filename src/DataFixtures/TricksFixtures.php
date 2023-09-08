7a1fec2f62b9b4568932534260145215ecca4bf4
BenjVA <ben.valette@live.fr>
2023-09-08 14:44:20 +0200

    WIPÂ trying to upload images

---
+++ src/DataFixtures/TricksFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Tricks;
+use App\Repository\UsersRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Faker\Factory;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class TricksFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function __construct(private SluggerInterface $slugger, private UsersRepository $usersRepository)
+    {
+
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $users = $this->usersRepository->findAll();
+
+        for ($trcks = 1; $trcks <= 10; $trcks++) {
+            $tricks = new Tricks();
+            $tricks->setName($faker->text(15));
+            $tricks->setDescription($faker->text());
+            $tricks->setSlug($this->slugger->slug(strtolower($tricks->getName())));
+            $tricks->setGroupTricks($faker->text(10));
+            $tricks->setUsers($users[mt_rand(0, 9)]);
+            $manager->persist($tricks);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UsersFixtures::class,
+        ];
+    }
+}
